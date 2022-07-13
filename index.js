52d273d80cd1e678a1daf3cd8c2e49857ab10c25
JH175 <JH175@protonmail.com>
2022-07-13 01:06:45 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let weightInput = document.querySelector('#weight-input');
+let tbsaInput = document.querySelector('#tbsa-input');
+let calcSubmit = document.querySelector('#calc-submit');
+let weightPrint = document.querySelector('#weight-print');
+let tbsaPrint = document.querySelector('#tbsa-print');
